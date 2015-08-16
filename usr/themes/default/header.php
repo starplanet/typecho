2cd46e6646af6eeb626748e82af7c8a3f1693219
zhangjinjie <zhangjinjie@yimian.com.cn>
2015-08-16 14:45:42 +0800

    add baidu authentication key

--- usr/themes/default/header.php
+++ usr/themes/default/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="baidu-site-verification" content="UbSyXl5eyy">
     <title><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
